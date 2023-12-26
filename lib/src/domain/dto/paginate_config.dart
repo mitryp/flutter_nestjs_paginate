4bb20c146d46d13708923adea49e65d47925cfc1
Dmytro Popov <thedmitryp@ukr.net>
2023-12-26 23:07:19 +0000

    Documented `PaginateConfig`

--- lib/src/domain/dto/paginate_config.dart
+++ lib/src/domain/dto/paginate_config.dart
@@ -11,18 +11,42 @@ part 'paginate_config.freezed.dart';
 
 part 'paginate_config.g.dart';
 
+/// A class mirroring the client-facing side of PaginateConfig of `nestjs-paginate`.
+///
+/// It omits backend- and database-specific fields, while preserving those needed on the frontend.
+///
+/// The config from the backend can be deserialized used [PaginateConfig.fromJson] which supports
+/// its syntax.
 @Freezed(toJson: false)
 class PaginateConfig with _$PaginateConfig {
   const factory PaginateConfig({
+    /// A set of columns allowed to be used in sorts.
+    ///
+    /// If strict validation is enabled, adding a sort with the column not in the set will cause
+    /// StateError. Otherwise, the sort will be ignored unless validation is disabled.
     @Default({}) @JsonKey(fromJson: _sortableColumnsFromJson) Set<String> sortableColumns,
+
+    /// A set of columns allowed for filtering by.
+    ///
+    /// If strict validation is enabled, adding a filter with the column not in the set will cause
+    /// StateError. Otherwise, the filter will be ignored unless validation is disabled.
     @Default({})
     @JsonKey(fromJson: _filterableColumnsFromJson)
     Map<String, Set<Type>> filterableColumns,
+
+    /// A maximum limit accepted by the server.
+    /// A controller will not increase its limit over this value.
     @Default(100) int maxLimit,
+
+    /// A limit to be set by default.
     @Default(20) int defaultLimit,
+
+    /// The default sorts applied when no sorts are configured in a controller.
     @JsonKey(fromJson: _defaultSortByFromJson) Map<String, SortOrder>? defaultSortBy,
   }) = _PaginateConfig;
 
+  /// Deserializes [PaginateConfig] from JSON received from a NextJS server. It can handle edge
+  /// cases, such as {'column': true} in `filterableColumns` and other TS moments.
   factory PaginateConfig.fromJson(Map<String, Object?> json) => _$PaginateConfigFromJson(json);
 }
 
