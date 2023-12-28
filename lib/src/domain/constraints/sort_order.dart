f318e999f8afdc0de2196f04185a59a8a0ceab1a
Dmytro Popov <thedmitryp@ukr.net>
2023-12-28 19:43:17 +0000

    Added docs to `SortOrder`

--- lib/src/domain/constraints/sort_order.dart
+++ lib/src/domain/constraints/sort_order.dart
@@ -1,12 +1,18 @@
+/// An enumeration of {asc, desc}.
 enum SortOrder {
   asc,
   desc;
 
+  /// Returns the opposite [SortOrder].
   SortOrder flipped() => switch (this) {
         SortOrder.asc => SortOrder.desc,
         SortOrder.desc => SortOrder.asc,
       };
 
+  /// Finds a [SortOrder] with the given [name].
+  /// The name will be trimmed and lowercased before the search.
+  ///
+  /// If no sort order with such a name exists, throws a [StateError].
   static SortOrder fromName(String name) {
     final normalizedName = name.trim().toLowerCase();
 
