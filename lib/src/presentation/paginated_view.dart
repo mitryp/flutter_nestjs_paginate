7a72bad10b865954e4a40d72caac441171c524ed
Dmytro Popov <thedmitryp@ukr.net>
2023-12-28 18:23:54 +0000

    Added docs to PaginatedView; removed _controller field

--- lib/src/presentation/paginated_view.dart
+++ lib/src/presentation/paginated_view.dart
@@ -11,7 +11,6 @@ typedef ErrorBuilder = Widget Function(BuildContext context, Object? error);
 typedef PaginatedViewBuilder<TModel> = Widget Function(
   BuildContext context,
   Paginated<TModel> data,
-  PaginationController controller,
 );
 
 typedef PaginatedDataFetcher<TModel> = FutureOr<Paginated<TModel>> Function(
@@ -19,16 +18,49 @@ typedef PaginatedDataFetcher<TModel> = FutureOr<Paginated<TModel>> Function(
   QueryParams params,
 );
 
+/// A widget providing a framework for building controllable paginated data views powered by
+/// `nestjs-paginate` on backend. It combines together the [controller] for pagination,
+/// [viewBuilder] for building the presentation of the data of type [TModel] received from a
+/// [fetcher]. It handles displaying of errors with [errorBuilder] and shows a [loadingIndicator].
+///
+/// Components:
+/// - [controller] provides reactive controls for the query, notifying this widget of
+/// its updates.
+/// - [fetcher] is a function taking [BuildContext] and [QueryParams]  (Map<String, Object?>) and
+/// returning a [Paginated] of [TModel] or its Future.
+/// - [viewBuilder] is a builder of the presentation of the current page data received from the
+/// [fetcher].
+/// - [errorBuilder] is a builder which will be used in the event of [fetcher] throwing.
+/// - [loadingIndicator] is a builder which will be used while the data from the [fetcher] is
+/// loading.
 class PaginatedView<TModel> extends StatefulWidget {
+  /// A [PaginationController] of this [PaginatedView].
+  /// This widget will rebuild whenever the controller sends notifications.
   final PaginationController controller;
-  final PaginatedViewBuilder<TModel> builder;
+
+  /// A builder of the presentation of the current page data received from the [fetcher].
+  final PaginatedViewBuilder<TModel> viewBuilder;
+
+  /// A builder of the error message.
+  /// It is used when the [fetcher] throws an error.
   final ErrorBuilder errorBuilder;
-  final PaginatedDataFetcher<TModel> fetcher;
+
+  /// A widget that ish shown when the data from fetcher is being loaded.
   final WidgetBuilder loadingIndicator;
 
+  /// A function that takes [QueryParams] built by the controller and [BuildContext] and returns
+  /// [Paginated]<[TModel]>.
+  final PaginatedDataFetcher<TModel> fetcher;
+
+  /// Creates a [PaginatedView] with the parameters provided.
+  ///
+  /// [controller] updates will make this widget update its data and call the [fetcher] with the
+  /// result of [PaginationController.toMap].
+  ///
+  /// See the fields docs for more info.
   const PaginatedView({
     required this.controller,
-    required this.builder,
+    required this.viewBuilder,
     required this.errorBuilder,
     required this.fetcher,
     required this.loadingIndicator,
@@ -40,7 +72,6 @@ class PaginatedView<TModel> extends StatefulWidget {
 }
 
 class _PaginatedViewState<TModel> extends State<PaginatedView<TModel>> {
-  late PaginationController _controller = widget.controller;
   late Paginated<TModel> _data;
   Object? _error;
   bool _isLoading = false;
@@ -49,7 +80,7 @@ class _PaginatedViewState<TModel> extends State<PaginatedView<TModel>> {
   void initState() {
     super.initState();
 
-    _controller.addListener(_updateData);
+    widget.controller.addListener(_updateData);
     _updateData();
   }
 
@@ -58,15 +89,15 @@ class _PaginatedViewState<TModel> extends State<PaginatedView<TModel>> {
     super.didUpdateWidget(oldWidget);
 
     if (oldWidget.controller != widget.controller) {
-      _controller.removeListener(_updateData);
+      oldWidget.controller.removeListener(_updateData);
       widget.controller.addListener(_updateData);
-      setState(() => _controller = widget.controller);
+      _updateData();
     }
   }
 
   @override
   void dispose() {
-    _controller.removeListener(_updateData);
+    widget.controller.removeListener(_updateData);
 
     super.dispose();
   }
@@ -75,7 +106,7 @@ class _PaginatedViewState<TModel> extends State<PaginatedView<TModel>> {
     setState(() => _isLoading = true);
 
     try {
-      _data = await widget.fetcher(context, _controller.toMap());
+      _data = await widget.fetcher(context, widget.controller.toMap());
     } catch (err) {
       _error = err;
     }
@@ -97,6 +128,6 @@ class _PaginatedViewState<TModel> extends State<PaginatedView<TModel>> {
       return widget.errorBuilder(context, error);
     }
 
-    return widget.builder(context, _data, _controller);
+    return widget.viewBuilder(context, _data);
   }
 }
