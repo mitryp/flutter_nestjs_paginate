98cd984852fd6e98ce402f39b7bc24e5fc65daa1
Dmytro Popov <thedmitryp@ukr.net>
2024-01-01 20:37:01 +0000

    Added exports of the public API

--- lib/flutter_nestjs_paginate.dart
+++ lib/flutter_nestjs_paginate.dart
@@ -1,1 +1,11 @@
 library flutter_nestjs_paginate;
+
+export 'src/application/pagination_controller.dart' show PaginationController;
+export 'src/domain/constraints/filter_operator.dart'
+    show Ilike, In, Eq, FilterOperator, Contains, Lt, Btw, Gt, Not, Null, Or, Sw;
+export 'src/domain/constraints/sort_order.dart' show SortOrder;
+export 'src/domain/dto/paginate_config.dart' show PaginateConfig;
+export 'src/domain/dto/paginated.dart' show PaginatedMetadata, Paginated;
+export 'src/domain/typedefs.dart' show QueryParams, FromJsonDecoder;
+export 'src/presentation/paginated_view.dart'
+    show PaginatedView, ErrorBuilder, PaginatedViewBuilder, PaginatedDataFetcher;
